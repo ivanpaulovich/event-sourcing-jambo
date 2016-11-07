2da6285b000a13b85eb27ff4cba64787831e0d1b
Ivan Paulovich <Ivan Paulovich>
2016-11-07 00:03:55 -0200

    'ok'

---
+++ Jambo/Jambo.Core.Services/Aggregates/Room.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jambo.Core.Services.Aggregates
+{
+    public class Room
+    {
+    }
+}
