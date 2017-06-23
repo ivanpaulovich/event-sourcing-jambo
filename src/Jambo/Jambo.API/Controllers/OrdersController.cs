d782e7f6b32526f58bdf2b934b89ffca78633c20
Ivan Paulovich <ivan@100loop.com>
2017-06-23 15:10:38 -0300

    ok

--- src/Jambo/Jambo.API/Controllers/OrdersController.cs
+++ src/Jambo/Jambo.API/Controllers/OrdersController.cs
@@ -18,24 +18,5 @@ namespace Jambo.API.Controllers
 
             _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
-
-<<<<<<< HEAD
-        [Route("new")]
-        public async Task<IActionResult> CreateOrder()//[FromBody]CreateOrderCommand command, [FromHeader(Name = "x-requestid")] string requestId)
-        {
-            CreateOrderCommand command = new CreateOrderCommand();
-=======
-        //[Route("new")]
-        //[HttpPost]
-        //public async Task<IActionResult> CreateOrder()//[FromBody]CreateOrderCommand command, [FromHeader(Name = "x-requestid")] string requestId)
-        //{
-        //    CreateOrderCommand command = new CreateOrderCommand();
->>>>>>> 099be64974c6460c11d9bb9b75fc9da35c27ce6e
-
-        //    bool commandResult = await _mediator.Send(command);
-
-        //    return commandResult ? (IActionResult)Ok() : (IActionResult)BadRequest();
-
-        //}
     }
 }
