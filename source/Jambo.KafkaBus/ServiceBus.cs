2644075e82dd34ecec11295b1f81cbb2cb1273f1
Ivan Paulovich <ivan.gomes@oleconsignado.onmicrosoft.com>
2017-08-09 12:45:40 -0300

    ok

--- source/Jambo.KafkaBus/ServiceBus.cs
+++ source/Jambo.KafkaBus/ServiceBus.cs
@@ -1,6 +1,7 @@
 ﻿using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
 using Jambo.Domain.SeedWork;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,29 +12,31 @@ namespace Jambo.KafkaBus
     public class ServiceBus : IServiceBus
     {
         private readonly string _topicName;
-        private readonly Producer<Null, string> _producer;
-        private readonly Consumer<Null, string> _consumer;
+        private readonly Producer<string, string> _producer;
+        private readonly Consumer<string, string> _consumer;
 
         public ServiceBus(string brokerList, string topicName)
         {
             _topicName = topicName;
 
-            _producer = new Producer<Null, string>(
+            _producer = new Producer<string, string>(
                 new Dictionary<string, object>() {{
                     "bootstrap.servers", brokerList
-                }}, null, new StringSerializer(Encoding.UTF8));
+                }}, new StringSerializer(Encoding.UTF8), new StringSerializer(Encoding.UTF8));
 
-            _consumer = new Consumer<Null, string>(
+            _consumer = new Consumer<string, string>(
                 new Dictionary<string, object>() {
                 { "group.id", "simple-csharp-consumer" },
                 { "bootstrap.servers", brokerList }},
-                null, new StringDeserializer(Encoding.UTF8));
+                new StringDeserializer(Encoding.UTF8), new StringDeserializer(Encoding.UTF8));
         }
 
         public async Task Publish(IEvent _event)
         {
-            //await Task.Run(() => Console.WriteLine(_event.ToString()));
-            Message<Null, string> message = await _producer.ProduceAsync(_topicName, null, DateTime.Now.ToString() + _event.ToString());
+            string data = JsonConvert.SerializeObject(_event);
+
+            Message<string, string> message = await _producer.ProduceAsync(
+                _topicName, _event.ToString(), data);
         }
 
         public Task Listen()
@@ -45,7 +48,7 @@ namespace Jambo.KafkaBus
 
             while (true)
             {
-                Message<Null, string> msg;
+                Message<string, string> msg;
 
                 if (_consumer.Consume(out msg, TimeSpan.FromSeconds(1)))
                 {
