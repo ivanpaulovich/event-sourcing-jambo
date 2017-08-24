24f63a054b4e69872a33b5c90a3537afa07b1f80
Ivan Paulovich <ivan.gomes@oleconsignado.onmicrosoft.com>
2017-08-24 16:46:57 -0300

    ok

--- source/Jambo.Web/Index.cshtml.cs
+++ source/Jambo.Web/Index.cshtml.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Jambo.Application.Queries;
 
 namespace Jambo.Web
 {
     public class IndexModel : PageModel
     {
+        private readonly IBlogQueries _blogQueries;
+
+        public IndexModel(IBlogQueries blogQueries)
+        {
+            _blogQueries = blogQueries ?? throw new ArgumentNullException(nameof(blogQueries));
+        }
+
         public void OnGet()
         {
         }
