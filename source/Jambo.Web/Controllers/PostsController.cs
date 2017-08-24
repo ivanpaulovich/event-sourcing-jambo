e6c3692506f5909196d28c5accde614da9646eeb
Ivan Paulovich <ivan.gomes@oleconsignado.onmicrosoft.com>
2017-08-24 14:01:26 -0300

    ok

---
+++ source/Jambo.Web/Controllers/PostsController.cs
@@ -0,0 +1,131 @@
+﻿using Jambo.Application.Commands;
+using Jambo.Application.Commands.Blogs;
+using Jambo.Application.Commands.Posts;
+using Jambo.Application.Queries;
+using Jambo.Domain.Aggregates.Blogs;
+using Jambo.Domain.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Jambo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class PostsController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly IPostQueries _postQueries;
+
+        public PostsController(IMediator mediator, IPostQueries postQueries)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+            _postQueries = postQueries ?? throw new ArgumentNullException(nameof(postQueries));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPosts(Guid blogId)
+        {
+            var posts = await _postQueries.GetPostsAsync(blogId);
+
+            return Ok(posts);
+        }
+
+        [HttpGet("{id}", Name = "GetPost")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            try
+            {
+                var post = await _postQueries.GetPostAsync(id);
+
+                return Ok(post);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]CreatePostCommand command)
+        {
+            Guid id = await _mediator.Send(command);
+
+            return CreatedAtRoute("GetPost", new { id = id }, id);
+        }
+
+        [HttpPatch("Enable")]
+        public async Task<IActionResult> Enable([FromBody]EnablePostCommand command)
+        {
+            try
+            {
+                await _mediator.Send(command);
+                return (IActionResult)Ok();
+            }
+            catch (BlogDomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("Disable")]
+        public async Task<IActionResult> Disable([FromBody]DisablePostCommand command)
+        {
+            try
+            {
+                await _mediator.Send(command);
+                return (IActionResult)Ok();
+            }
+            catch (BlogDomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("Publish")]
+        public async Task<IActionResult> Publish([FromBody]PublishPostCommand command)
+        {
+            try
+            {
+                await _mediator.Send(command);
+                return (IActionResult)Ok();
+            }
+            catch (BlogDomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("Hide")]
+        public async Task<IActionResult> Hide([FromBody]HidePostCommand command)
+        {
+            try
+            {
+                await _mediator.Send(command);
+                return (IActionResult)Ok();
+            }
+            catch (BlogDomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPatch("UpdateContent")]
+        public async Task<IActionResult> UpdateContent([FromBody]UpdatePostContentCommand command)
+        {
+            try
+            {
+                await _mediator.Send(command);
+                return (IActionResult)Ok();
+            }
+            catch (BlogDomainException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ source/Jambo.Web/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Jambo.Domain.ServiceBus;
+using Newtonsoft.Json;
+using MediatR;
+using Jambo.Application.Commands.Blogs;
+using Autofac;
+using Jambo.API.IoC;
+using Autofac.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Jambo.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        IServiceProvider serviceProvider;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+            services.AddMediatR(typeof(CreateBlogCommand).GetTypeInfo().Assembly);
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Blogging HTTP API",
+                    Version = "v1",
+                    Description = "The Blogging Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+
+            ContainerBuilder container = new ContainerBuilder();
+            container.Populate(services);
+
+            container.RegisterModule(new MediatorModule());
+
+            container.RegisterModule(new ApplicationModule(
+                Configuration.GetSection("MongoDB").GetValue<string>("ConnectionString"),
+                Configuration.GetSection("MongoDB").GetValue<string>("Database")));
+
+            container.RegisterModule(new ServiceBusModule(
+                Configuration.GetSection("ServiceBus").GetValue<string>("ConnectionString"),
+                Configuration.GetSection("ServiceBus").GetValue<string>("Topic"),
+                ProcessDomainEventDelegate));
+
+            serviceProvider = new AutofacServiceProvider(container.Build());
+
+            return serviceProvider;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        private void ProcessDomainEventDelegate(string topic, string key, string value)
+        {
+            Type eventType = Type.GetType(key);
+            DomainEvent domainEvent = (DomainEvent)JsonConvert.DeserializeObject(value, eventType);
+
+            serviceProvider.GetService<IMediator>().Publish(domainEvent).Wait();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger()
+               .UseSwaggerUI(c =>
+               {
+                   c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+               });
+        }
+    }
+}
